d82e34522fb52b0bb791b051f4791cedc328d110
Aleksei Pirogov <alekseipirogovQA@yandex.ru>
2025-11-10 21:50:29 +0300

    Add FileParser class

---
+++ src/FileParser.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class FileParser {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int fileCounter = 0;
+
+                while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String filePath = scanner.nextLine();
+
+            File file = new File(filePath);
+            boolean fileExists = file.exists();
+            boolean isFile = file.isFile();
+
+            if (!fileExists || !isFile) {
+                if (!fileExists) {
+                    System.out.println("Файл не существует: " + filePath);
+                } else {
+                    System.out.println("Указанный путь ведет к папке, а не к файлу: " + filePath);
+                }
+                continue;
+            }
+
+            fileCounter++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + fileCounter);
+        }
+    }
+}
